36c8d86423be5c2fd74b1720f18c5a0b3cc1a7f1
Rafael <rafaelmagalhaes1@live.com>
2023-09-15 10:47:40 -0300

    update comments

--- menu-lateral.php
+++ menu-lateral.php
@@ -6,14 +6,14 @@ Version: 2.0
 Author: Rafael M.
 */
 
-// Adicione os estilos e scripts necessários
+// Estilos e scripts necessários
 function menu_lateral_fixo_enqueue_scripts() {
     wp_enqueue_style('menu-lateral-fixo-styles', plugin_dir_url(__FILE__) . 'css/menu.css');
     wp_enqueue_script('menu-lateral-fixo-scripts', plugin_dir_url(__FILE__) . 'js/menu.js', array('jquery'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'menu_lateral_fixo_enqueue_scripts');
 
-// Renderize o menu de ícones
+// Renderiza o menu de ícones
 function menu_lateral_fixo_render_menu() {
     //
     $plugin_dir = plugin_dir_url(__FILE__);
